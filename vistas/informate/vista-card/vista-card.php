4d5a030e63ea9e7c5486c0c3fe731297ff7752a2
Juangarzon11 <juangarzon07@gmail.com>
2020-11-04 18:02:02 -0500

    Vista card

---
+++ vistas/informate/vista-card/vista-card.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VistaCard</title>
+    
+<!--importacion boostrap-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;900&display=swap" rel="stylesheet">
+
+<!-- Font Awesome -->
+<script src="https://kit.fontawesome.com/f599362e7b.js" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="../../../css/estilos.css">
+</head>
+<body>
+<section class="">
+        <nav class="navbar navbar-expand-lg navbar-light bg-nav">
+            <div class="col-sm-3 text-center">
+                <a class="navbar-brand" href="../../../index.php">
+                    <img src="../../../img/logo.png" alt="">
+                </a>
+            </div>
+
+            <button class="navbar-toggler col-sm-3 ml-auto" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav hola ml-auto">
+                    <li class="nav-item ">
+                        <a class="nav-link" href="index.php">Inicio</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="vistas/conocenos.php">Conócenos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Informate</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Eventos-Tour</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Aliados</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Material Turistico</a>
+                    </li>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-invi dropdown-toggle" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false">
+                            Perfil
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg-left">
+                            <button class="dropdown-item" type="button">Mi Perfil</button>
+                            <button class="dropdown-item" type="button">Configuración</button>
+                            <div class="dropdown-divider"></div>
+                            <button class="dropdown-item" type="button">Cerrar Sesión</button>
+                        </div>
+                    </div>
+                </ul>
+            </div>
+        </nav>
+    </section>
+
+    <section>
+        <div class="contenedor col-12"> 
+            <div class="row col-12 c-img" style="background-image: url( '../../../img/bg-01.jpg')">
+                <h1 class="title">esternocleidomastoideo</h1>
+            </div>
+        
+        </div>
+            <!-- <div>
+                <div class="c-img ">
+                    <img class="d-block w-100 sli-img" src="../../../img/slid-01.jpg">
+                    <div class="txt">
+                        <h1>CAPURGANA</h1>
+                    </div>
+                </div>
+            </div> -->
+    </section>
+    
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+</body>
+</html>
