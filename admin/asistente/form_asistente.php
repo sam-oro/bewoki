5370e021feee3dcec99013d39842bbcbe6b96196
sam-oro <samiroroz@gmail.com>
2020-11-05 18:09:41 -0500

    Formulario con estilos

--- admin/asistente/form_asistente.php
+++ admin/asistente/form_asistente.php
@@ -1,42 +1,49 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>asistente</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="../estilos/estilos.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <!-- Styles -->
+    <link rel="stylesheet" href="../../css/estilos.css">
+
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,700&display=swap" rel="stylesheet">
 
     <!-- Ionic icons -->
     <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
 
-
-    
+    <title>Asistente</title>
 </head>
-    <body>
 
-    <div class="d-flex" id="content-wrapper">
+<body>
+
+    <div class="d-flex" id="conten-prin">
         <!-- Sidebar -->
-        <div id="sidebar-container" class="bg-primary">
-            <div class="logo">
-                <h4 class="text-light font-weight-bold mb-0">Templune</h4>
+        <div id="sidebar-container" class="col-3">
+            <div class="col-3 d-none d-lg-block">
+                <img src="../../img/isologo-blan.png" alt="">
             </div>
             <div class="menu">
-                <a href="form_asistente" class="d-block text-light p-3 border-0"><i class="icon ion-md-apps lead mr-2"></i>asistente</a>
+                <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-apps lead mr-2"></i> <p class="d-none d-lg-block">asistente</p> </a>
                 <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-people lead mr-2"></i>Usuarios</a>
                 <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-stats lead mr-2"></i>Estadísticas</a>
                 <a href="#" class="d-block text-light p-3 border-0"><i class="icon ion-md-person lead mr-2"></i>Perfil</a>
                 <a href="#" class="d-block text-light p-3 border-0"> <i class="icon ion-md-settings lead mr-2"></i>Configuración</a>
             </div>
         </div>
-
-    <div class="container col-8">
-        <div class="row col-8">
-            <form action="guardar_asistente.php" method="POST" class="col-8">
+        <div class="container formularios col-9">
+            <form action="guardar_asistente.php" method="POST" class="col-12">
+                <div>
+                    <h3>Asistente</h3>
+                </div>
                 <div class="form-group">
-                    <label>identificacion</label>
+                    <label>identificación</label>
                     <input type="text" class="form-control" name="id">
                 </div>
                 <div class="form-group">
@@ -44,34 +51,31 @@
                     <input type="text" class="form-control" name="nombre">
                 </div>
                 <div class="form-group">
-                    <label>contraseña</label>
+                    <label>Contraseña</label>
                     <input type="password" class="form-control" name="password">
                 </div>
                 <div class="form-group">
-                    <label>movil</label>
+                    <label>Celular</label>
                     <input type="text" class="form-control" name="celular">
                 </div>
                 <div class="form-group">
-                    <label>correo</label>
+                    <label>Correo</label>
                     <input type="email" class="form-control" name="correo">
                 </div>
-                <button type="submit" class="btn btn-primary">enviar</button>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-color text-center">Enviar</button>
+                </div>
+
             </form>
         </div>
     </div>
 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js" integrity="sha256-R4pqcOYV8lt7snxMQO/HSbVCFRPMdrhAFMH+vr9giYI=" crossorigin="anonymous"></script>
+</body>
 
-    
-    
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
-
-
-    </body>
-        
 </html>
