b4d346eb1647a627ecdb963323c9d447b8018c58
Asfgm 123 <sgiraldo776@misena.edu.co>
2020-11-06 11:01:24 -0500

    index Listo

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 <script src="https://kit.fontawesome.com/f599362e7b.js" crossorigin="anonymous"></script>
 
 <link rel="stylesheet" href="css/estilos.css">
+<link rel="stylesheet" href="css/estilos2.css">
 
 <body>
     <section class="">
@@ -89,65 +90,157 @@
             </a>
         </div>
     </section>
-    <hr>
-    <section class="video_index">
-        <div class="video_idx">
-            <?php
-            include "admin/conexion/conexion.php";
+    <div class="container">
+        <hr>
+        <section class="container">
+            <div class="video_idx">
+                <iframe class="video" src="https://www.youtube.com/embed/bUsq4jHt3eY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <!--<?php/*
+                include "admin/conexion/conexion.php";
 
-            $baseDeDatos = obtenerBaseDeDatos();
-            $coleccion = $baseDeDatos->video_index;
-            $cursor = $coleccion->find(); // Realiza la busueda del video
+                $baseDeDatos = obtenerBaseDeDatos();
+                $coleccion = $baseDeDatos->video_index;
+                $cursor = $coleccion->find(); // Realiza la busueda del video
 
-            foreach ( $cursor as $id => $valor )
-            {
-                $row=json_decode(json_encode($valor),true);
-                echo('<iframe class="video" src="'.$row['Url'].'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
-            }
-            ?>
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                Cambiar Video
-            </button>
+                foreach ( $cursor as $id => $valor )
+                {
+                    $row=json_decode(json_encode($valor),true);
+                    echo('<iframe class="video" src="'.$row['Url'].'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+                }*/
+                ?>
+                
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Cambiar Video
+                </button>
 
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Cambiar Video</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Cambiar Video</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="container">
+                                    <form action="admin/video_idx/actu_video.php" method="post" enctype="multipart/form-data">
+                                        <div class="form-group">
+                                            <label>Nombre</label>
+                                            <input class="form-control" type="text" name="nombre">
+                                        </div>
+                                        <fieldset>
+                                        <legend>video</legend>
+                                        <p>Si inserta el video con un URL no es necesario que seleccione un video desde su dispositivo y viceversa; Si rellena el campo URL y selecciona un archivo desde su dispositivo, se dará prioridad al URL que ingreso primero</p>
+                                        <div class="form-group">
+                                            <label>URL Video</label>
+                                            <input class="form-control" type="text" name="url">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Seleccionar Video</label>
+                                            <input class="form-control" type="file" name="video">
+                                        </div>
+                                        </fieldset>
+                                        <button type="submit" class="btn btn-primary">enviar</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="modal-body">
-                        <div class="container">
-                            <form action="admin/video_idx/actu_video.php" method="post" enctype="multipart/form-data">
-                                <div class="form-group">
-                                    <label>Nombre</label>
-                                    <input class="form-control" type="text" name="nombre">
-                                </div>
-                                <fieldset>
-                                <legend>video</legend>
-                                <p>Si inserta el video con un URL no es necesario que seleccione un video desde su dispositivo y viceversa; Si rellena el campo URL y selecciona un archivo desde su dispositivo, se dará prioridad al URL que ingreso primero</p>
-                                <div class="form-group">
-                                    <label>URL Video</label>
-                                    <input class="form-control" type="text" name="url">
-                                </div>
-                                <div class="form-group">
-                                    <label>Seleccionar Video</label>
-                                    <input class="form-control" type="file" name="video">
-                                </div>
-                                </fieldset>
-                                <button type="submit" class="btn btn-primary">enviar</button>
-                            </form>
+                </div> --> 
+            </div>
+        </section>
+        <hr>
+        <section class="container">
+            <div class="que_es">
+                <div class="titulos">
+                    <h1>Que es Bewoki</h1>
+                </div>
+                <div class="contenido_que">
+                    <div class="row fila_cont">
+                        <div class="col-md-6 col-sm-12">
+                            <h3 class="titulo">¿Quienes somos?</h3>
+                            <p>Somos la comunidad digital para descubrir, emocionar y compartir experiencias y contenido saludable, seguro y sostenible en el territorio.</p>
+                        </div>
+                        <div class="col-md-6 col-sm-12">
+                            <h3 class="titulo">¿Qué hacemos?</h3>
+                            <p>Visibilizamos sobre el territorio y conectamos la oferta y demanda de contenido y experiencias turísticas, culturales, creativas, salud y bienestar.</p>
+                        </div>
+                    </div>
+                    <div class="row fila_cont">
+                        <div class="col-md-6 col-sm-12">
+                            <h3 class="titulo">¿Cómo lo hacemos?</h3>
+                            <p>A través de una plataforma tecnológica innovadora eficiente e incluyente que permite conformar una comunidad digital colaborativa.</p>
+                        </div>
+                        <div class="col-md-6 col-sm-12">
+                            <h3 class="titulo">¿Para quién lo hacemos?</h3>
+                            <p>A través de una plataforma tecnológica innovadora eficiente e incluyente que permite conformar una comunidad digital colaborativa.</p>
+                        </div>
+                    </div>
+                    <div class="row fila_cont">
+                        <h3 class="titulo">¿Qué beneficios obtiene el usuario, cliente o consumidor?</h3>
+                        <p>1. Seguridad y eficiencia en el descubrir, disfrutar y compartir experiencias y contenidos en el territorio.</p>
+                        <p>2. Salud y bienestar físico, emocional y mental.</p>
+                        <p>3. Consciencia sostenible mediante una conexión social con el patrimonio natural y cultural.</p>
+                        <p>4. Posibilidad de generar y aumentar ingresos, gracias al modelo de recompensas, marketing de afiliados y conversión de leads.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <hr>
+        <section class="container">
+            <div class="valores">
+                <div class="titulos">
+                    <h1>Nuestros Valores</h1>
+                </div>
+                <div class="contenido_valores">
+                    <div class="row fila_cont">
+                        <div class="col-md-4 col-sm-12 img">
+                            <img class="img_valores" src="img/iconos-01.svg" alt="">
+                        </div>
+                        <div class="col-md-8 col-sm-12">
+                            <p><b>• Integridad:</b> Estamos comprometidos a hacer lo que es correcto, honesto y justo.</p>
+                            <p><b>• Calidad:</b> Nos dedicamos a producir consistentemente resultados que cumplan y excedan las expectativas de nuestros clientes.</p>
+                            <p><b>• Respeto:</b> Valoramos las marcas y sus productos, haciendo alianzas estratégicas con diferentes empresas del medio.</p>
+                            <p><b>• Competitividad e innovación:</b> Constantemente ideamos proyectos para mejorar los procesos, producción y la calidad de los bienes o los servicios.</p>
+                            <p><b>• Goce y Disfrute:</b> Disfrutamos y nos gozamos cada uno de los proyectos que hacemos. Somos una empresa feliz.</p>
                         </div>
                     </div>
                 </div>
             </div>
+        </section>
+        <hr>
+        <section class="container">
+            <div class="tarjetas">
+                    <div class="row fila_cont">
+                        <div class="col-md-3 col-sm-12 text-center">
+                            <a href="#">
+                                <img class="img_valores" src="img/index-icon-01.svg" alt="">
+                                <h3 class="titulo">Unete a la comunidad</h3>
+                            </a>
+                        </div>
+                        <div class="col-md-3 col-sm-12 text-center">
+                            <a href="#">
+                                <img class="img_valores" src="img/index-icon-02.svg" alt="">
+                                <h3 class="titulo">Ayudados a mejorar</h3>
+                            </a>
+                        </div>
+                        <div class="col-md-3 col-sm-12 text-center">
+                            <a href="#">
+                                <img class="img_valores" src="img/index-icon-03.svg" alt="">
+                                <h3 class="titulo">Conoce Colombia</h3>
+                            </a>
+                        </div>
+                        <div class="col-md-3 col-sm-12 text-center">
+                            <a href="vistas/contacto/contactenos.php">
+                                <img class="img_valores" src="img/index-icon-04.svg" alt="">
+                                <h3 class="titulo">Contactanos</h3>
+                            </a>
+                        </div>
+                    </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
